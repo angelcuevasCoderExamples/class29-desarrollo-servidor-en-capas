18afd4758f1d2449a29d36799f594906265d487e
angelcuevas <angel.p.cuevas@gmail.com>
2024-04-16 20:55:02 -0300

    added: empty routers to app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { port } = require('./config/config');
+const { usersRouter } = require('./routes/users.router');
+const { businessRouter } = require('./routes/business.router');
+const { ordersRouter } = require('./routes/orders.router');
 
 const app = express();
 
+
+/** ROUTES */
+app.use('/api/users', usersRouter);
+app.use('/api/business', businessRouter);
+app.use('/api/orders', ordersRouter);
 
 
 app.listen(port, ()=>console.log(`Server up and running on port ${port}`))
