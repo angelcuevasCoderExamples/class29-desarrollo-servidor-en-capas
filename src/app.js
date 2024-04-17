ab0d28173c26e9aa77075a60a1acd310e452c352
angelcuevas <angel.p.cuevas@gmail.com>
2024-04-16 22:00:36 -0300

    added: mongo connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { port } = require('./config/config');
+const { port, mongoConnectionLink } = require('./config/config');
 const { usersRouter } = require('./routes/users.router');
 const { businessRouter } = require('./routes/business.router');
 const { ordersRouter } = require('./routes/orders.router');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect(mongoConnectionLink).then(()=>{
+    console.log('Connected to database')
+})
 
+/**middlewares */
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 /** ROUTES */
 app.use('/api/users', usersRouter);
