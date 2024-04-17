406857c823c371fb63f52c01cc56380a93820483
angelcuevas <angel.p.cuevas@gmail.com>
2024-04-16 21:07:17 -0300

    added: users controller

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,26 @@
+class UsersController {
+    static async getAll(req, res){
+        try {
+            res.send({status:'success', payload:'getAll'})
+        } catch (error) {
+            res.status(500).send({status:'error', error: error.message})
+        }
+    }
+    static async getById(req, res){
+        try {
+            res.send({status:'success', payload:'getById'})
+        } catch (error) {
+            res.status(500).send({status:'error', error: error.message})
+        }
+    }
+    static async create(req, res){
+        try {
+            res.send({status:'success', payload:'create'})
+        } catch (error) {
+            res.status(500).send({status:'error', error: error.message})
+        }
+    }
+    
+}
+
+module.exports = UsersController; 
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,7 +1,11 @@
 const {Router} = require('express');
+const UsersController = require('../controllers/users.controller');
 
 const router = Router(); 
 
+router.get('/', UsersController.getAll)
+router.get('/:id', UsersController.getById)
+router.post('/', UsersController.create)
 
 module.exports = {
     usersRouter: router
